c5d3ff2d8d19ee74faa25d7a0eeb9da1a0de3891
saulariasd <saul_ariasd@hotmail.com>
2020-03-14 23:42:50 -0500

    variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -9,7 +9,7 @@ process.env.PORT = process.env.PORT || 3000;
 //             ENTORNO
 //  ================================
 
-// process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 
 //  ================================
@@ -20,7 +20,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://saul:LJoMka7QLVx44Lx3@cluster0-79pcu.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
