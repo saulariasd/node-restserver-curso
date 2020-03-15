2ce20ffbd274af1cb008dc0bd59008d69390de77
saulariasd <saul_ariasd@hotmail.com>
2020-03-14 23:08:56 -0500

    restserver-usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,24 @@
 //  ================================ 
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+//  ================================
+//             ENTORNO
+//  ================================
+
+// process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+//  ================================
+//                 BD
+//  ================================
+
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://saul:LJoMka7QLVx44Lx3@cluster0-79pcu.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
