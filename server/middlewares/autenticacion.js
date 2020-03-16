c349cdedbc45421152e9eff42c3aa1039b5a512b
saulariasd <saul_ariasd@hotmail.com>
2020-03-16 12:33:52 -0500

    subida de archivos

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -22,6 +22,10 @@ let verificaToken = (req, res, next) => {
 
 };
 
+//===============================
+//verifica si el rol es administrador
+//===============================
+
 let verificaAdminRole = (req, res, next) => {
     let usuario = req.usuario;
     if (usuario.rol === 'ADMIN_ROLE') {
@@ -37,7 +41,30 @@ let verificaAdminRole = (req, res, next) => {
     }
 
 }
+
+//===============================
+//verifica token para imagen
+//===============================
+
+let verificaTokenImg = (req, res, next) => {
+    let token = req.query.token;
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'token no valido'
+                }
+            })
+        }
+        req.usuario = decoded.usuario;
+        next();
+    });
+}
+
+
 module.exports = {
     verificaToken,
     verificaAdminRole,
+    verificaTokenImg
 }
